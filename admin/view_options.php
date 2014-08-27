4b20e75b91e4c880e21fd9fa3de7cd42bfd98851
Leo Baiano <ljunior2005@gmail.com>
2014-08-27 08:47:50 -0300

    issue #6 - incluindo mensagem de sucesso ou erro quandos as configuracoes do plugin sao alteradas

--- admin/view_options.php
+++ admin/view_options.php
@@ -7,6 +7,12 @@
 	<h2>
 		<?php _e( 'Pimap Options', 'pimap' ); ?>
 	</h2>
+
+	<?php if( isset( $_GET['settings-updated'] ) && $_GET['settings-updated'] == 'true' ){ ?>
+		<div id="setting-error-settings_updated" class="updated settings-error"> 
+		<p><strong><?php _e( 'Saved settings', 'pimap' ); ?></strong></p></div>
+	<?php } ?>
+
 	<form method='post' action='options.php'>
 		<?php
 			settings_fields( 'pimap_settings' );
